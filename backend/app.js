a09a67d988dcaccaa410a58eecca6313ff8c5efa
AliMahmoud26 <alimah.engineer@gmail.com>
2026-03-11 16:20:50 +0200

    Connecting to database and creating the the Schema for the user model which will include the Email and Password of the user

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import {connectDB} from './config/db.js';
+
+dotenv.config();
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send("Hello World!");
 });
+
+connectDB();
 
 const port = process.env.PORT || 5000;
 
